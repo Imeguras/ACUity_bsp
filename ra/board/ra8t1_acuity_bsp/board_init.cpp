7e0c76a1e169af7a80fb1ba902e7cea3eeedba11
Imeguras <imeguras@cock.li>
2024-06-24 18:10:37 +0100

    Fix

--- ra/board/ra8t1_acuity_bsp/board_init.cpp
+++ ra/board/ra8t1_acuity_bsp/board_init.cpp
@@ -20,17 +20,19 @@ fsp_err_t R_IOPORT_OutputLogicFetch (ioport_ctrl_t * const p_ctrl,
 #endif
     bsp_io_port_t port    = (bsp_io_port_t) (IOPORT_PRV_PORT_BITS & (ioport_size_t) pin);
     ioport_size_t shift    = IOPORT_PRV_PIN_BITS & (ioport_size_t) pin;
-
+    ioport_size_t pin_mask = (ioport_size_t) (1U << shift);
+    //TODO check if this is necessary, from the surface it seems not to but the thing keeps crashing
+    R_BSP_PinAccessEnable();
 
     /* Get the port address */
     R_PORT0_Type * p_ioport_regs = RENESAS_IOPORT_PRV_PORT_ADDRESS((port >> RENESAS_IOPORT_PRV_PORT_OFFSET) & RENESAS_IOPORT_PRV_8BIT_MASK);
     //now lets get PODR
-    uint8_t level =  p_ioport_regs->PODR & shift;
-
+    uint16_t level =  (uint16_t)(p_ioport_regs->PODR & pin_mask);
     if(level){
         *value = BSP_IO_LEVEL_HIGH;
     }else{
         *value = BSP_IO_LEVEL_LOW;
     }
+    R_BSP_PinAccessDisable();
     return FSP_SUCCESS;
 }
